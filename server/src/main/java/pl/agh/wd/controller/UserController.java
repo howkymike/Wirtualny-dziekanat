a633b0c062ac6ddcf30b461a145a54915a2e1e97
meqeq <mekekwengiel25@gmail.com>
2021-04-14 20:14:21 +0200

    small fix

--- server/src/main/java/pl/agh/wd/controller/UserController.java
+++ server/src/main/java/pl/agh/wd/controller/UserController.java
@@ -2,6 +2,7 @@ package pl.agh.wd.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import pl.agh.wd.service.UserService;
 
 @RestController
 @RequestMapping("/api/user/{type}")
+@PreAuthorize("hasRole('ADMIN')")
 public class UserController {
 
     @Autowired
