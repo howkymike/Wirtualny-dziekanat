eee33a74259fa8d17dfd1ff36b4baab23d0c4ffc
mike <michal@jastrzebscy.org>
2021-04-15 11:15:26 +0200

    Admin platform info

--- server/src/main/java/pl/agh/wd/controller/AdminController.java
+++ server/src/main/java/pl/agh/wd/controller/AdminController.java
@@ -1,10 +1,24 @@
 package pl.agh.wd.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.agh.wd.payload.response.MessageResponse;
+import pl.agh.wd.payload.response.PlatformInfoResponse;
+import pl.agh.wd.repository.RoleRepository;
+import pl.agh.wd.repository.UserRepository;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Admin related API
@@ -17,9 +31,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/admin")
 public class AdminController {
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
     @GetMapping("/supersecret")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String adminSecret() {
         return "You are powerful now :D";
+    }
+
+    @GetMapping("/platforminfo")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<?> platformInfo() {
+        return ResponseEntity.ok(new PlatformInfoResponse());
     }
 }
---
+++ server/src/main/java/pl/agh/wd/payload/response/PlatformInfoResponse.java
@@ -0,0 +1,86 @@
+package pl.agh.wd.payload.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+/**
+ * Handles /api/admin/platforminfo response
+ *
+ * @author: howkymike
+ */
+
+@Getter
+public class PlatformInfoResponse {
+
+    private static final String PROP_MEMORY = "memory";
+    private static final String PROP_OS = "os";
+    private static final String PROP_THREADS = "threads";
+
+    private static final String PROP_MEMORY_INIT = "init";
+    private static final String PROP_MEMORY_USED = "used";
+    private static final String PROP_MEMORY_MAX = "max";
+    private static final String PROP_MEMORY_COMMITTED = "committed";
+
+    private String os;
+    private HashMap<String, Double> memoryMap;
+    private List<ThreadModel> threadList;
+
+
+    public PlatformInfoResponse() {
+        setOs();
+        setMemory();
+        setThreads();
+    }
+
+    private void setOs() {
+        this.os = System.getProperty("os.name");
+    }
+
+    private void setMemory() {
+        memoryMap = new HashMap<>(4);
+        MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
+        memoryMap.put(PROP_MEMORY_INIT, ((double)memoryMXBean.getHeapMemoryUsage().getInit() /1073741824));
+        memoryMap.put(PROP_MEMORY_USED,((double)memoryMXBean.getHeapMemoryUsage().getUsed() /1073741824));
+        memoryMap.put(PROP_MEMORY_MAX,((double)memoryMXBean.getHeapMemoryUsage().getMax() /1073741824));
+        memoryMap.put(PROP_MEMORY_COMMITTED,((double)memoryMXBean.getHeapMemoryUsage().getCommitted() /1073741824));
+    }
+
+    private void setThreads() {
+        threadList = new ArrayList<ThreadModel>();
+        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+        for(Long threadID : threadMXBean.getAllThreadIds()) {
+            ThreadInfo info = threadMXBean.getThreadInfo(threadID);
+            threadList.add(new ThreadModel(
+                    info.getThreadName(),
+                    info.getThreadState().toString(),
+                    threadMXBean.getThreadCpuTime(threadID)));
+        }
+    }
+
+    @Getter
+    @Setter
+    static class ThreadModel {
+        private static final String THREAD_NAME = "name";
+        private static final String THREAD_STATE = "state";
+        private static final String THREAD_CPU_TIME = "cpu_time";
+
+        private String name;
+        private String state;
+        private long cpu_time;
+
+        public ThreadModel(String name, String state, long cpu_time) {
+            this.name = name;
+            this.state = state;
+            this.cpu_time = cpu_time;
+        }
+    }
+}
