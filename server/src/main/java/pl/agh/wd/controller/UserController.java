791503be0869d62e43970eef4bac1d3b057b2bee
Jesiotr <sniegu703@gmail.com>
2021-04-15 23:05:53 +0200

    Added delete user api

--- server/src/main/java/pl/agh/wd/controller/UserController.java
+++ server/src/main/java/pl/agh/wd/controller/UserController.java
@@ -3,25 +3,23 @@ package pl.agh.wd.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import pl.agh.wd.model.User;
 import pl.agh.wd.payload.response.ListResponse;
+import pl.agh.wd.payload.response.MessageResponse;
 import pl.agh.wd.payload.response.SuccessResponse;
 import pl.agh.wd.service.UserService;
 
 @RestController
-@RequestMapping("/api/user/{type}")
-@PreAuthorize("hasRole('ADMIN')")
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    
-    @GetMapping("/")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/{type}")
     public ResponseEntity<?> index(@PathVariable("type") String type) {
 
         switch(type) {
@@ -37,5 +35,18 @@ public class UserController {
         }
 
         return ResponseEntity.ok(new SuccessResponse(false, "DDD"));
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long userId){
+        User user = userService.getUserById(userId);
+
+        if(user == null){
+            return  ResponseEntity.badRequest().body(new MessageResponse("User id is invalid."));
+        }else{
+            userService.deleteUser(user);
+            return ResponseEntity.ok(new MessageResponse("User deleted."));
+        }
     }
 }
--- server/src/main/java/pl/agh/wd/service/UserService.java
+++ server/src/main/java/pl/agh/wd/service/UserService.java
@@ -3,12 +3,13 @@ package pl.agh.wd.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import pl.agh.wd.model.Clerk;
-import pl.agh.wd.model.Professor;
-import pl.agh.wd.model.Student;
+import pl.agh.wd.model.*;
 import pl.agh.wd.repository.ClerkRepository;
 import pl.agh.wd.repository.ProfessorRepository;
 import pl.agh.wd.repository.StudentRepository;
+import pl.agh.wd.repository.UserRepository;
+
+import javax.validation.constraints.NotNull;
 
 @Service
 public class UserService {
@@ -20,6 +21,31 @@ public class UserService {
 
     @Autowired
     private ProfessorRepository professorRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User getUserById(Long id){
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public void deleteUser(@NotNull User user){
+        for (Role role : user.getRoles()) {
+            switch (role.getName()){
+                case ROLE_STUDENT:
+                    studentRepository.deleteById(user.getId());
+                    break;
+                case ROLE_LECTURER:
+                    professorRepository.deleteById(user.getId());
+                    break;
+                case ROLE_STUFF:
+                    clerkRepository.deleteById(user.getId());
+                    break;
+            }
+        }
+
+        userRepository.deleteById(user.getId());
+    }
     
     public Iterable<Student> getStudentList() {
         return studentRepository.findAll();
