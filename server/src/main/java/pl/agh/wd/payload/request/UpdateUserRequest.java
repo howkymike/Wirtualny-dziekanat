82b8a3a7c93699fb6fe1f4fc76edfdf238be78fc
Filip Sądowicz <sadowicz31@gmail.com>
2021-04-21 20:24:15 +0200

    [WD-21/WD-68] Adding UpdateUserRequest preview

---
+++ server/src/main/java/pl/agh/wd/payload/request/UpdateUserRequest.java
@@ -0,0 +1,55 @@
+package pl.agh.wd.payload.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+@Getter
+@Setter
+public class UpdateUserRequest {
+
+    @NotBlank
+    private long id;
+
+    @NotBlank
+    @Size(min = 5, max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 5, max = 20)
+    private String name;
+
+    @NotBlank
+    @Size(min = 5, max = 20)
+    private String surname;
+
+    @NotBlank
+    private String city;
+
+    @NotBlank
+    private String country;
+
+    @NotBlank
+    private String postalCode;
+
+    @NotBlank
+    private String telephone;
+
+    @NotBlank
+    private String address;
+
+    private Set<String> role;
+
+    @NotBlank
+    @Size(min = 8, max = 40)
+    private String password;
+}
