0d22d14290e94b7ae2a88f7486e596f129fa04d7
mike <michal@jastrzebscy.org>
2021-04-24 16:34:22 +0200

    Animation test

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -34,6 +34,11 @@ const Box = styled.div`
     justify-content: space-around;
     align-items: center;
     box-shadow: 0 4px 5px 0 rgba(0, 0, 0, .14), 0 1px 10px 0 rgba(0, 0, 0, .12), 0 2px 4px -1px rgba(0, 0, 0, .20);
+    
+    &:hover {
+        transform: scale(1.1);
+    }
+    transition: transform .2s;
 `;
 
 const Home = props => {
