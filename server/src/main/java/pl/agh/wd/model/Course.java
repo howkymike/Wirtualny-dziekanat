2a327c8e53fea164f2b10a8bcb5fd9e5ccaaade8
Filip Sądowicz <sadowicz31@gmail.com>
2021-05-10 20:27:34 +0200

    [WD-121] Fixed Deleting all students from course.

--- server/src/main/java/pl/agh/wd/model/Course.java
+++ server/src/main/java/pl/agh/wd/model/Course.java
@@ -3,6 +3,8 @@ package pl.agh.wd.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
 
 import javax.persistence.*;
 import java.util.Set;
@@ -36,6 +38,7 @@ public class Course {
     private String name;
 
     @OneToMany(mappedBy = "course")
+    @NotFound(action = NotFoundAction.IGNORE)
     private Set<CourseStudent> courseStudents;
 
     @ManyToMany
