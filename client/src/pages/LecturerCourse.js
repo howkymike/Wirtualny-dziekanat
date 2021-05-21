1bb41a58a1c2b2d45a1042c760e76134585f2da6
mike <michal@jastrzebscy.org>
2021-05-21 14:15:14 +0200

    Lecturer now can add students to course

--- client/src/pages/LecturerCourse.js
+++ client/src/pages/LecturerCourse.js
@@ -5,10 +5,11 @@ import { Wrapper } from './StudentCourse';
 
 import { userContext } from '../context/userContext'
 import ImprovedGradientButton from '../components/ImprovedGradientButton';
+import CourseModal from "../components/EditCourseComponent/CourseModal";
 
 const LecturerCourse = props => {
     const { fetchApi } = useContext(userContext)
-
+    const [modal, setModal] = useState([false, 0, "edit"]);
     const [loading, setLoading] = useState(true);
     const [list, setList] = useState([]);
 
@@ -43,12 +44,21 @@ const LecturerCourse = props => {
                                 <td>{course.fieldOfStudy.name}</td>
                                 <td>{course.fieldOfStudy.faculty.name}</td>
                                 <td>{course.courseStudents.length}</td>
-                                <td><ImprovedGradientButton>Szczegóły</ImprovedGradientButton></td>
+                                <td><ImprovedGradientButton>Szczegóły</ImprovedGradientButton><div className='mt-1' />
+                                    <ImprovedGradientButton onClick={ () => setModal([!modal[0], course.id, "edit"]) }>Dodaj studenta</ImprovedGradientButton>
+                                </td>
                             </tr>
                         )}
                 </tbody>
             </Table>
-
+            { modal[0] &&
+            <CourseModal
+                isOpen={ modal[0] }
+                id={ modal[1] }
+                toggle={ () => setModal([!modal[0], modal[1], modal[2]]) }
+                type={modal[2]}
+            />
+            }
 
         </Wrapper>
 
