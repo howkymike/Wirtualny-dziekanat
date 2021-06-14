bbc9020b4403a6e8c52dcc3a95ebb607b90aceaf
FEJTWOW <kamil.wierciak99@gmail.com>
2021-06-14 16:03:18 +0200

    Tests

--- test.sh
+++ test.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-docker-compose -f docker-compose.test.yml up
+docker-compose -f docker-compose.test.yml up 
+./run.sh &
+sleep 60
+cd tests/e2e && npm install codeceptjs playwright --save && npx codeceptjs run --steps
